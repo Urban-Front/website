ed7be2b26569600148ec1fc797a854e96fa0e3b0
skiprox <seanmichaelscanlan@gmail.com>
2020-09-15 15:27:04 -0400

    Work on menu

--- src/components/site/Header.js
+++ src/components/site/Header.js
@@ -1,44 +1,47 @@
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
+import Nav from 'components/site/Nav';
+import ScrollLockComponent from 'components/utils/scrollLockComponent';
 
 export default function Header({preview = false, className = ''}) {
+  const [burgerState, setBurgerState] = useState(false);
+  useEffect(() => {
+    // Whenever we hit the escape key,
+    // close the burger menu (if open)
+    const trackEscapeKey = e => {
+      if (e.key === 'Escape') {
+        setBurgerState(false);
+      }
+    };
+    window.addEventListener('keyup', trackEscapeKey);
+    return () => {
+      window.removeEventListener('keyup', trackEscapeKey);
+    };
+  }, [setBurgerState]);
   return (
-    <header className={`Header w-100 ${className}`}>
-      <article className="Container flex items-center justify-between">
-        <section className="Header__logo">
-          <Link href={`/${preview ? '?preview=true' : ''}`}>
-            <a title="Home">
-              <img className="Header__logo-image db" src="/images/uf_logo.png" alt="" />
-            </a>
-          </Link>
-        </section>
-        <ul className="Header__nav flex flex-column flex-row-md">
-          <li className="Header__nav-item dib mr3">
-            <Link href={`/about${preview ? '?preview=true' : ''}`}>
-              <a title="About" className="Header__nav-link">About</a>
+    <>
+      <header className={`Header w-100 ${className}`}>
+        <article className="Container flex items-center justify-between">
+          <section className="Header__logo">
+            <Link href={`/${preview ? '?preview=true' : ''}`}>
+              <a title="Home">
+                <img className="Header__logo-image db" src="/images/uf_logo.png" alt="" />
+              </a>
             </Link>
-          </li>
-          <li className="Header__nav-item dib mr3">
-            <Link href={`/team${preview ? '?preview=true' : ''}`}>
-              <a title="Team" className="Header__nav-link">Team</a>
-            </Link>
-          </li>
-          <li className="Header__nav-item dib mr3">
-            <Link href={`/voices${preview ? '?preview=true' : ''}`}>
-              <a title="Voices" className="Header__nav-link">Voices</a>
-            </Link>
-          </li>
-          <li className="Header__nav-item dib mr3">
-            <Link href={`/events${preview ? '?preview=true' : ''}`}>
-              <a title="Events" className="Header__nav-link">Events</a>
-            </Link>
-          </li>
-          <li className="Header__nav-item dib">
-            <Link href={`/contact${preview ? '?preview=true' : ''}`}>
-              <a title="Contact" className="Header__nav-link">Contact</a>
-            </Link>
-          </li>
-        </ul>
-      </article>
-    </header>
+          </section>
+          <section className="Header__hamburger">
+            <button
+              className="Header__hamburger-button"
+              onClick={() => {setBurgerState(!burgerState)}}
+            >Menu</button>
+          </section>
+        </article>
+      </header>
+      <Nav
+        toggleNav={burgerState}
+        setToggle={setBurgerState}
+      />
+      {burgerState && <ScrollLockComponent />}
+    </>
   );
 };
