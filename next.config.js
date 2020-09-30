6c56d3c5a5cb62186fbcd2c31f0c02ff53ffcc5f
skiprox <seanmichaelscanlan@gmail.com>
2020-09-29 18:09:41 -0700

    Getting rid of purgecss

--- next.config.js
+++ next.config.js
@@ -2,74 +2,25 @@
 const withPlugins = require('next-compose-plugins')
 const withTM = require('next-transpile-modules')
 const withSass = require('@zeit/next-sass')
-const withPurgeCss = require('next-purgecss')
 
 require('dotenv').config()
 
 // With purge css
 const prodConfig = withPlugins(
   [withTM],
-  withSass(
-    withPurgeCss({
-      target: 'serverless',
-      distDir: '.next',
-      env: {
-        CONTENTFUL_SPACE:
-          process.env.CONTENTFUL_SPACE,
-        CONTENTFUL_TOKEN:
-          process.env.CONTENTFUL_TOKEN,
-        CONTENTFUL_PREVIEW_TOKEN:
-          process.env.CONTENTFUL_PREVIEW_TOKEN
-      },
-      purgeCssPaths: [
-        'src/pages/**/*',
-        'src/components/**/*',
-        'src/svg/**/*'
-      ],
-      purgeCss: {
-        whitelist: () => [
-          'a',
-          'absolute',
-          'p',
-          'em',
-          'b',
-          'i',
-          'blockquote',
-          'swiper',
-          'blue',
-          'red',
-          'green',
-          'orange',
-          'purple',
-          'swiper-container',
-          'swiper-container-fade',
-          'swiper-container-initialized',
-          'swiper-container-horizontal',
-          'swiper-container-vertical',
-          'swiper-wrapper',
-          'swiper-slide',
-          'swiper-slide-duplicate',
-          'swiper-slide-active',
-          'swiper-slide-next',
-          'swiper-slide-prev',
-          'swiper-slide-duplicate-next',
-          'swiper-slide-duplicate-prev',
-          'swiper-button-next',
-          'swiper-button-prev',
-          'swiper-button-disabled',
-          'swiper-lazy-preloader',
-          'swiper-lazy',
-          'swiper-lazy-loaded',
-          'page-transition-enter',
-          'page-transition-enter-done',
-          'page-transition-enter-active',
-          'page-transition-exit',
-          'page-transition-exit-active'
-        ]
-      }
-    })
-  )
-)
+  withSass({
+    target: 'serverless',
+    distDir: '.next',
+    env: {
+      CONTENTFUL_SPACE:
+        process.env.CONTENTFUL_SPACE,
+      CONTENTFUL_TOKEN:
+        process.env.CONTENTFUL_TOKEN,
+      CONTENTFUL_PREVIEW_TOKEN:
+        process.env.CONTENTFUL_PREVIEW_TOKEN
+    }
+  })
+);
 
 const devConfig = withPlugins(
   [withTM],
