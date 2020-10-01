4964bfa30a273e81cb0469a386d9a0a027fc1ee1
skiprox <seanmichaelscanlan@gmail.com>
2020-09-30 19:36:58 -0700

    Updating with video

--- src/components/modules/Notice.js
+++ src/components/modules/Notice.js
@@ -7,6 +7,9 @@ export default function Notice({
 }) {
   return (
     <article className={`Notice tc pv6 flex flex-column justify-center items-center ${className}`}>
+      <video className="Notice__video absolute" autoPlay loop muted playsInline poster="images/poster.png">
+        <source src="video/sequence.mp4" type="video/mp4" />
+      </video>
       <div className="Container Container__medium tl">
         <h1 className="Notice__header special-header">{data.fields.title}</h1>
       </div>
@@ -16,7 +19,7 @@ export default function Notice({
         </div>
         <div className="Notice__cta mt3">
           <Link href={`/${data.fields.ctaLink}`}>
-            <a title={data.fields.ctaText}>{data.fields.ctaText}</a>
+            <a title={data.fields.ctaText}>{data.fields.ctaText} ›</a>
           </Link>
         </div>
       </div>
