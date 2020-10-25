6c87f4ac6d0c315eea20ed4b48e4d2573b1c389e
skiprox <seanmichaelscanlan@gmail.com>
2020-10-25 10:29:36 -0700

    Work on filtering on voices page

--- src/pages/voices/index.js
+++ src/pages/voices/index.js
@@ -12,59 +12,152 @@ export default function Voices(context) {
   const [preview, setPreview] = useState(false);
   const [data, setData] = useState(context.articles);
   const [articleTypes, setArticleTypes] = useState([]);
+  const [articleTags, setArticleTags] = useState([]);
+  const [activeFilters, setActiveFilters] = useState({
+    types: [],
+    tags: []
+  });
   getPreviewData(getArticles, setData, setPreview, false);
+
+  /**
+   * Add or remove a filter from activeFilters
+   */
+  const addOrRemoveFilter = (filterName, type) => {
+    const exists = activeFilters[type].includes(filterName);
+    if (exists) {
+      if (type === 'tags') {
+        const newArr = activeFilters.tags.filter((c) => {return c !== filterName});
+        setActiveFilters({types: activeFilters.types, tags: newArr});
+      } else {
+        const newArr = activeFilters.types.filter((c) => {return c !== filterName});
+        setActiveFilters({types: newArr, tags: activeFilters.tags});
+      }
+    } else {
+      if (type === 'tags') {
+        const newArr = activeFilters.tags;
+        newArr.push(filterName);
+        setActiveFilters({types: activeFilters.types, tags: newArr});
+      } else {
+        const newArr = activeFilters.types;
+        newArr.push(filterName);
+        setActiveFilters({types: newArr, tags: activeFilters.tags});
+      }
+    }
+  }
+
   useEffect(() => {
     let allArticleTypes = [];
+    let allArticleTags = [];
+    /**
+     * Create the type and tag filters arrays
+     */
     data.forEach(item => {
       if (allArticleTypes.indexOf(item.fields.type[0]) === -1) {
         allArticleTypes.push(item.fields.type[0]);
       }
+      if (item.fields.tags) {
+        item.fields.tags.forEach(tag => {
+          if (allArticleTags.indexOf(tag) === -1) {
+            allArticleTags.push(tag);
+          }
+        });
+      }
     });
     setArticleTypes([...new Set(allArticleTypes)]);
+    setArticleTags([...new Set(allArticleTags)]);
   }, []);
   return (
     <Layout preview={preview}>
       <article className={`Voices ph3 w-100`}>
-        <h3 className="body-header tc mb3 mb6-lg">UF Voices</h3>
-        <section className="Voices__container Container Container__medium flex mv3 mv4-lg">
-          <ul className="Voices__filters flex justify-between">
-            <li className="Voices__filter Voices__filter-header dib">Filter:</li>
+        <section className="Voices__container Container mv3 mv4-lg">
+          <h3 className="body-header">Filters</h3>
+          <ul className="Voices__filters flex flex-wrap mt3">
             {articleTypes.map((item, index) => {
               return (
-                <li className="Voices__filter Voices__filter-button dib" key={index}><button>{item}</button></li>
+                <li className="Voices__filter" key={index}>
+                  <button
+                    className={classnames('Voices__filter-button dib mr3 pa2', {
+                      active: activeFilters.types.indexOf(item) !== -1
+                    })}
+                    onClick={() => {
+                      addOrRemoveFilter(item, 'types');
+                    }}
+                  >{item}</button>
+                </li>
+              )
+            })}
+          </ul>
+          <ul className="Voices__filters flex flex-wrap mt3">
+            {articleTags.map((item, index) => {
+              return (
+                <li className="Voices__filter" key={index}>
+                  <button
+                    className={classnames('Voices__filter-button dib mr3 pa2', {
+                      active: activeFilters.tags.indexOf(item) !== -1
+                    })}
+                    onClick={() => {
+                      addOrRemoveFilter(item, 'tags');
+                    }}
+                  >{item}</button>
+                </li>
               )
             })}
           </ul>
         </section>
-        <section className="Voices__container Container Container__medium flex flex-wrap mb6-lg">
-          {data.map((item, index) => {
-            return (
-              <section key={index} className="Voices__article flex flex-column flex-row-md justify-between w-100 mv3">
-                {item.fields.image && (
-                  <figure className="w-100 w-50-md mb3 mb0-md mr0 mr3-md">
-                    <img className="Voices__image" src={`${item.fields.image.fields.file.url}?w=320&fm=jpg&q=70`} />
-                  </figure>
-                )}
-                <div className={classnames('w-100', {
-                  'w-50-md': item.fields.image
-                })}>
-                  <h5 className="Voices__type">{item.fields.type[0]}</h5>
-                  <h3 className="Voices__title">{item.fields.title}</h3>
-                  {item.fields.tags && (
-                    <ul className="Voices__tags">
-                      {item.fields.tags.map((item, index) => {
-                        return (<li className="Voices__tag dib mr1" key={index}>{item}</li>)
-                        })}
-                    </ul>
-                  )}
-                  <p className="Voices__snippet mt3 body-copy">{item.fields.snippet}</p>
-                  <Link as={`/voices/${item.fields.slug}`} href="/voices/[slug]">
-                    <a className="dib mt3">Read More →</a>
-                  </Link>
-                </div>
-              </section>
-            )
-          })}
+        <section className="Voices__container Container mb4 mb6-lg">
+          <div className="Voices__container-inner flex flex-wrap">
+            {data.filter(item => {
+              if (activeFilters.types.length || activeFilters.tags.length) {
+                let filterBool = false;
+                if (activeFilters.types.length) {
+                  // check if any types from the item
+                  // are in the activeFilters.types,
+                  // return true if so
+                  filterBool = activeFilters.types.indexOf(item.fields.type[0]) !== -1;
+                }
+                if (activeFilters.tags.length) {
+                  // check if any tags from the item
+                  // are in the activeFilters.tags,
+                  // return true if so
+                  let tags = item.fields.tags || [];
+                  tags.forEach(tag => {
+                    filterBool = filterBool || activeFilters.tags.indexOf(tag) !== -1;
+                  });
+                }
+                return filterBool;
+              } else {
+                return true;
+              }
+            }).map((item, index) => {
+              return (
+                <Link as={`/voices/${item.fields.slug}`} href="/voices/[slug]" key={index}>
+                  <a className="Voices__article-link flex w-100 w-third-lg  db">
+                    <div className="Voices__article flex-grow-1 flex flex-column ma3 pa3">
+                      {item.fields.image && (
+                        <figure className="w-100 mb3 mb0-md mr0 mr3-md">
+                          <img className="Voices__image db" src={`${item.fields.image.fields.file.url}?w=320&fm=jpg&q=70`} />
+                        </figure>
+                      )}
+                      <div className="w-100">
+                        <h5 className="Voices__type mt2">{item.fields.type[0]}</h5>
+                        <h3 className="Voices__title mt1">{item.fields.title}</h3>
+                        {item.fields.author && (
+                          <h5 className="Voices__author mt2">by {item.fields.author}</h5>
+                        )}
+                        {item.fields.tags && (
+                          <ul className="Voices__tags">
+                            {item.fields.tags.map((item, index) => {
+                              return (<li className="Voices__tag dib mr1" key={index}>{item}</li>)
+                              })}
+                          </ul>
+                        )}
+                      </div>
+                    </div>
+                  </a>
+                </Link>
+              )
+            })}
+          </div>
         </section>
       </article>
     </Layout>
