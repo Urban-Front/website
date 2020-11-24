1a9c6c85df1ad65b59cca2c9402bdcb3845c4774
skiprox <seanmichaelscanlan@gmail.com>
2020-11-23 17:52:53 -0800

    Updating notices to do better checking for type of background media, starting work on bg color

--- src/components/modules/Notice.js
+++ src/components/modules/Notice.js
@@ -1,20 +1,22 @@
 import { useState } from 'react';
 import Link from 'next/link';
+import classnames from 'classnames';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 export default function Notice({
   data,
   className = ''
 }) {
-  const [isVideo, setIsVideo] = useState(data.fields.backgroundMedia && data.fields.backgroundMedia.fields.file.contentType === 'video/mp4');
+  const isBgVideo = data.fields.backgroundMedia && data.fields.backgroundMedia.fields.file.contentType.split('/')[0] === 'video';
+  const isBgImage = data.fields.backgroundMedia && data.fields.backgroundMedia.fields.file.contentType.split('/')[0] === 'image';
   return (
     <article
       className={`Notice relative tc pv6 flex flex-column justify-center items-center ${className}`}
       style={{
-        backgroundImage: !isVideo ? `url(${data.fields.backgroundMedia.fields.file.url}?w=1600&fm=jpg&fl=progressive&q=70)`: 'none'
+        backgroundImage: isBgImage ? `url(${data.fields.backgroundMedia.fields.file.url}?w=1600&fm=jpg&fl=progressive&q=70)`: 'none'
       }}
     >
-      {isVideo && (
+      {isBgVideo && (
         <video className="Notice__video absolute" autoPlay loop muted playsInline poster="images/poster.png">
           <source src="video/sequence.mp4" type="video/mp4" />
         </video>
